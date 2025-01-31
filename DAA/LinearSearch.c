231740f1b33e8be3b0476a25bae108b132783dee
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2025-01-31 12:51:11 +0530

    daa

---
+++ DAA/LinearSearch.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+    int array[] = {1,2,3,4,5,6,7,8,9};
+    int n = sizeof(array)/ sizeof(array[0]);
+    int key, found, i = 0;
+    
+    printf("enter which number you wish to search: ");
+    scanf("%d", &key);
+    
+    for (i=0; i<n; i++){
+        if(array[i]==key){
+            printf("element found at index %d\n", i);
+            found = 1;
+            break;
+          
+        
+            
+        } else {
+        printf("Element not found in the array.\n");
+        break;
+    }
+    
+    }
+       
+
+    return 0;
+}
