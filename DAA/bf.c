5c4699c7b814787d3e2a1ee8d6ed391137344533
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2025-05-01 23:36:23 +0530

    c progs daa

--- DAA/bf.c
+++ DAA/bf.c
@@ -1,30 +1,26 @@
 #include <stdio.h>
+int main(){
+    int arr[] = {5,6,7,4,1};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int flag;
 
-int main() {
-    int arr[] = {5, 1, 4, 2, 8};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    int swapped;
-
-    for (int i = 0; i < n - 1; i++) {
-        swapped = 0;
-        for (int j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
+    for(int i=0; i<n-1;i++){
+        flag=0;
+        for(int j=0; j<n-i-1;j++){
+            if(arr[j]>arr[j+1]){
                 int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-                swapped = 1;
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+                flag=1;
             }
         }
-
-        
-        if (!swapped)
-            break;
+        if(!flag)
+        break;
     }
-
-    printf("Sorted array (optimized): ");
-    for (int i = 0; i < n; i++)
-        printf("%d ", arr[i]);
+    printf("sorted array: ");
+    for(int i=0;i<n;i++){
+        printf("%d", arr[i]);
     printf("\n");
-
+    }
     return 0;
 }
